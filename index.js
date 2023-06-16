5e91269c5e69a060853e3b912d23a28eb318e7ca
ecabott <ayush.bhusal0@gmail.com>
2023-06-16 14:11:50 +0545

    learned about operators

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ console.log(b);
 //using const
 const c = 789;
 console.log(c);
+
+//  alert('hmm');
+//  const age = prompt('age?');
+//  console.log(age);
+
+let x = 1;
+let y = 2;
+if (x == 1){
+    console.log('x is 1');
+}
+
+if (x == 1 && y == 2)
+{
+    console.log('&& operator tested');
+}
