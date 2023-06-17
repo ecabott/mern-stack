55fd714205f0dcdadab82492ba6772ba568ff3cc
ecabott <ayush.bhusal0@gmail.com>
2023-06-17 19:17:10 +0545

    calculating sum if different and tripling sum if same

--- js-exercise.js
+++ js-exercise.js
@@ -3,3 +3,13 @@ console.log('Hello World');
 
 //Getting value of pi
 console.log(Math.PI);
+
+//Computing sum if two integers are different and tripling their sum if they are same
+const a = Number(prompt('Enter integer a'));
+const b = Number(prompt('Enter integer b'));
+if(a == b){
+    console.log('Triple their sum is '+(a+b)*3);
+}
+else{
+    console.log('Their sum is '+(a+b));
+}
