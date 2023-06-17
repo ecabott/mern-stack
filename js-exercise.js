fb2d5d222ba7644f3134979bea9a8552e80c01a7
ecabott <ayush.bhusal0@gmail.com>
2023-06-17 19:21:01 +0545

    temperature conversion

--- js-exercise.js
+++ js-exercise.js
@@ -38,3 +38,11 @@ const mnd = (a, b) =>
     console.log(a/b);
 }
 mnd(d, e);
+
+//Conversion between Celsius & Fahrenheit
+const oldC = Number(prompt('Enter temperature in ℃'));
+const oldF = Number(prompt('Enter temperature in ℉'));
+const newC = ((oldF-32)/9)*5;
+const newF = ((oldC/5)*9)+32;
+console.log(oldC+'℃ is '+newF+'℉');
+console.log(oldF+'℉ is '+newC+'℃');
