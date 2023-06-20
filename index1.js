2e28123a8985f6bf115a2c5863cacc1f91075c5f
ecabott <ayush.bhusal0@gmail.com>
2023-06-20 13:58:05 +0545

    playing with arrays

--- index1.js
+++ index1.js
@@ -1,4 +1,5 @@
 //learning about object and functions
+console.log('Object and function');
 const person = {
     firstName: "John",
     lastName: "Doe",
@@ -10,3 +11,19 @@ const person = {
 const c = person.firstName;
 const a = person.fullName();
 console.log(c, a);
+
+//Arrays
+console.log('Arrays');
+const arr = ['Bugatti', 'BMW', 'Porshe', {name:'alto'}];
+//access the first item
+const b = arr[0];
+console.log(b);
+//access the last item
+const e = arr[arr.length - 1];
+console.log(e);
+
+console.log(arr.length);
+console.log(arr.sort());
+console.log(arr.push('new car'));
+console.log(arr.pop());
+
