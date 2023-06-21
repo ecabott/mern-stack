e2c23d63c9724f0fe39b092f346d4cc96ecf7286
ecabott <ayush.bhusal0@gmail.com>
2023-06-21 14:25:16 +0545

    installed and used node package

--- node-example/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const currentDateTime = require('./myFirstModule')
-
-http.createServer ((req, res) => {
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World!'+ currentDateTime.myDateTime);
-}).listen(8080);
-console.log('App running on port 8080');
---
+++ node-test/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const currentDateTime = require('./myFirstModule')
+const uc = require('upper-case');
+
+http.createServer ((req, res) => {
+    res.writeHead(200,{'Content-Type':'text/html'});
+    const message = uc.upperCase('hello world');
+    res.end(message + currentDateTime.myDateTime());
+}).listen(8080);
+console.log('App running on port 8080');
