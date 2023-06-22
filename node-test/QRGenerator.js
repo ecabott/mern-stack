733d2a2f6791e52b2ff9b70151d306d76c41c5fd
ecabott <ayush.bhusal0@gmail.com>
2023-06-22 14:36:26 +0545

    QRGen

---
+++ node-test/QRGenerator.js
@@ -0,0 +1,8 @@
+const QRCode = require('qrcode');
+
+QRCode.toDataURL('I am a pony!', function (err, url) {
+  console.log(url);
+});
+QRCode.toString('I am a pony!',{
+  type:'terminal'}, function (err, url) {console.log(url);
+});
