e1fce09bf1d7c1e7372460d1c634f1d6b2628d60
ecabott <ayush.bhusal0@gmail.com>
2023-06-27 20:22:09 +0545

    commit

--- node-exercise/weather.js
+++ node-exercise/weather.js
@@ -1,11 +1,28 @@
 const axios = require('axios');
 
+let apiData;
+const ddata= [
+    {
+        name:'ayush',
+        age:44,
+        address:'ayush',
+    },
+    {
+        name:'diwas',
+        age:44,
+        address:'ayush',
+    },
+]
 const fetchWeatherData = async () => {
-
+    const data = axios.get('https://jsonplaceholder.typicode.com/todos/1').then((data)=>{
+        console.log(data.data.title)
+        return data.data;
+    });
+  
 };
 
 async function getWeather() {
 
 };
 
-getWeather();
+fetchWeatherData();
