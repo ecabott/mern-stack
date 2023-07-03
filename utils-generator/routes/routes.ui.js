79d64aea04e8e49a02d4aa70062174f37b8bfda8
ecabott <ayush.bhusal0@gmail.com>
2023-07-03 22:20:49 +0545

    slight modification

--- utils-generator/routes/routes.ui.js
+++ utils-generator/routes/routes.ui.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const CC = require("currency-converter-lt");
 
+// const secureUI = (req, res, next) => {
+//   console.log("i am middleware");
+//   next();
+// };
+
 router.get("/", (req, res) => {
   res.render('index');
 });
@@ -21,5 +26,22 @@ router.get("/gallery", (req, res) => {
 router.get("/services", (req, res) => {
   res.render('services');
 });
+
+router.get(
+  "/converter/:currency1/:currency2/:value",
+  // secureUI,
+  async (req, res) => {
+    const { currency1, currency2, value } = req.params;
+    const currencyConverter = new CC({
+      from: currency1,
+      to: currency2,
+      amount: Number(value),
+    });
+    const result = await currencyConverter.convert();
+    res.send(
+      `Currency Price of ${value} ${currency1} in ${currency2} is ${result}`
+    );
+  }
+);
 
 module.exports = router;
