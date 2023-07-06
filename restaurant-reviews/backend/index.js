752f26767f157605104a795a9dd8e381ad50bb40
ecabott <ayush.bhusal0@gmail.com>
2023-07-06 21:48:04 +0545

    spelling correction

--- restaurant-reviews/backend/index.js
+++ restaurant-reviews/backend/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-import RestaurantsDAO from "./dao/RestaurantsDAO.js";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
