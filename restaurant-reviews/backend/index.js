8ce3ff061deb01c3e32f968437c39bcbf20fac32
ecabott <ayush.bhusal0@gmail.com>
2023-07-06 12:28:46 +0545

    updated

--- restaurant-reviews/backend/index.js
+++ restaurant-reviews/backend/index.js
@@ -1,5 +1,5 @@
 import app  from "./server.js"
-import mongodb, { MongoClient } from "mongodb"
+import mongodb from "mongodb"
 import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
